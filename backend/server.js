c61280667565a96a38360189d30ec5dbc42dc7e6
Nitin <nitinsharmaop828@gmail.com>
2026-07-14 15:40:22 +0530

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ import { Server } from 'socket.io';
 import {YSocketIO} from "y-socket.io/dist/server"
 
 const app=express();
+app.use(express.static("public"))
 const httpServer=createServer(app)
+
+
 
 const io= new Server(httpServer,{
     cors:{
@@ -16,20 +19,12 @@ const io= new Server(httpServer,{
 const ySocketIO= new YSocketIO(io)
 ySocketIO.initialize()
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message:"Hello world",
-        success: true
-    })
-})
-
 app.get("/health",(req,res)=>{
      res.status(200).json({
         message:"OK",
         success:true
      })
 })
-
 
 httpServer.listen(3000,()=>{
     console.log("Server is running")
